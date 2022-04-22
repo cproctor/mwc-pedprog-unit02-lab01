22c2d2aef352d4c040e34e151088fffd600c5cce
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-04-22 15:20:36 -0400

    Initial commit, rebasing

---
+++ promotion.py
@@ -0,0 +1,17 @@
+# part2.py
+# Author: [YOUR NAME HERE]
+#
+# This file should contain a Python program which generates a Bank and Account objects to match the state described in the lab spec sheet.
+
+from bank import Bank
+
+def promotion():
+    """
+    Function which creates the state of Banks and Accounts described in the lab spec webpage.
+    """
+
+    # 💻 YOUR CODE HERE
+
+
+if __name__ == "__main__":
+    promotion()
